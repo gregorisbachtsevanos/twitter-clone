9bc005c5d60c14cdc3861fde41a697225ad51ff8
Gregoris Bachtsevanos <gregorbachts@gmail.com>
2022-07-19 17:35:03 +0300

    file uploader part 1

---
+++ public/javascripts/uploader.js
@@ -0,0 +1,10 @@
+// import Uppy from '/@uppy/core'
+// import XHRUpload from '/@uppy/xhr-upload'
+// import Dashboard from '/@uppy/dashboard'
+// import Webcam from '@uppy/webcam'
+
+// var uppy = new Uppy.Core()
+// uppy.use(Uppy.DragDrop, { target: '#image-uploader' })
+// uppy.use(Uppy.Tus, { endpoint: 'https://tusd.tusdemo.net/files/' })
+
+// export { uppy };
